3103b0f3a7c71b2bf46cfe95e874e838461530c7
lopez9117 <lu.lopez@misena.edu.co>
2018-01-18 12:23:44 -0500

    agregando cors

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::group(['prefix' => 'v1'], function(){
+Route::group(['prefix' => 'v1','middleware' => 'cors'], function(){
 
 
 	Route::get('/', 'ContentController@index')->name('index');
